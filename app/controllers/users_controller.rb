ef14aa04803a8ce974705829ca1d18d68dc627c5
yamato090 <yamauchi09012@gmail.com>
2021-10-25 12:50:41 +0000

    add コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
     @book = Book.new
     @books = Book.all
-    @user = current_user
   end
 
   def index
@@ -13,22 +13,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find[params[:id]]
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:complete]=" updated."
       redirect_to user_path(current_user)
     else
-      @users = User.all
-      render :show
+      render :edit
     end
   end
   private
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
   def user_params
    	params.require(:user).permit(:name,:profile_image,:introduction)
   end
