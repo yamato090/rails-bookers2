62c8937c90dd6eb9c4310e2c42b464a9290cd0e0
yamato090 <yamauchi09012@gmail.com>
2021-10-25 12:18:10 +0000

    add コード修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,47 +1,48 @@
 class BooksController < ApplicationController
    def index
-     @user = current_user
-     @book = Book.new
-     @books = Book.all
+       @user = current_user
+       @book = Book.new
+       @books = Book.all
    end
    def create
-      @book = Book.new(book_params)
-      @book.user_id = current_user.id
-      if @book.save
-         flash[:complete]=" created."
-         redirect_to book_path(@book)
-      else
-         @books = Book.all
-         render :index
-      end
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        if @book.save
+           flash[:complete]=" created."
+           redirect_to book_path(@book)
+        else
+           @books = Book.all
+           render :index
+        end
    end
 
    def show
-     @book = Book.find(params[:id])
-     @books = Book.all
-     @user = current_user
+       @book = Book.find(params[:id])
+       @books = Book.all
+       @user = current_user
    end
 
    def edit
-      @book = Book.new
-      @book = Book.find(params[:id])
+       @book = Book.new
+       @book = Book.find(params[:id])
    end
    def update
-   @book = Book.find(params[:id])
-      if @book.update(book_params)
-        flash[:complete]=" updated."
-        redirect_to book_path(@book.id)
-      else
+        @book = Book.find(params[:id])
+        @book.user_id = current_user.id
+        if @book.update(book_params)
+          flash[:complete]=" updated."
+          redirect_to book_path(@book.id)
+        else
         @books = Book.all
-        render :edit
-      end
+        render :index
+        end
    end
    def destroy
-     book = Book.find(params[:id])
-      if book.destroy
-        flash[:complete]=" destroyed."
-        redirect_to books_path
-      end
+        book = Book.find(params[:id])
+        if book.destroy
+           flash[:complete]=" destroyed."
+           redirect_to books_path
+        end
    end
 
 
