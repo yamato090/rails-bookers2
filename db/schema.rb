ec0a29ff2ac2acea7a17bfe8268ba5b0d0da26d3
yamato090 <yamauchi09012@gmail.com>
2021-10-27 09:09:17 +0000

    add　コード修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_015345) do
+ActiveRecord::Schema.define(version: 2021_10_26_073320) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_015345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
